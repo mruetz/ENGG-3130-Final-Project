901a4b7b9a606ec123743e645757e5694a3c15b7
mruetz <56307805+mruetz@users.noreply.github.com>
2020-03-17 18:28:08 -0400

    Add files via upload

---
+++ MapQuestFunctions.py
@@ -0,0 +1,47 @@
+#Taken from the MapQuest Directions API documentation.
+
+#This file contains MapQuest functions that could be valuable.
+
+from RouteOptions import RouteOptions
+from AdvancedRouteOptions import AdvancedRouteOptions
+from RouteShapeOptions import RouteShapeOptions
+from RouteService import RouteService
+
+if __name__ == '__main__':
+    options = RouteOptions()
+    service = RouteService('YOUR_MAPQUEST_DEVELOPER_API_KEY') # our key is "dAGkCaPVqA3OcC5ws2Lfv8wsR9ro45oe"
+
+    location_list = ['Lancaster, PA', 'York, PA'] #First convert given addresses to Lat/Long values.
+    #latLon values can be given in list form using JSONs
+    
+    # eg.  location_list=[{'latLng':{'lng':43.488369,'lat':-80.207687}},{'latLng':{'lng':43.63656,'lat':-79.39722}}]
+
+    multi_location_list = ['Lancaster, PA', 'State College, PA', 'York, PA']
+
+    # directions
+    dirRoute1 = service.directions(locations=['Lancaster, PA', 'York, PA'])
+
+    dirRoute2 = service.directions(locations=['Lancaster, PA', 'York, PA'], options=options)
+
+    dirRoute3 = service.directions(locations=location_list)
+    
+    multiDirRoute = service.directions(locations=multi_location_list, options=options)
+    
+    # alternate route
+    
+    #This function can be used to get a SET of possible routes, which can be compared using
+    #additional factors like load weight applied.
+    altRoute = service.alternateRoute(locations=['Lancaster, PA', 'York, PA'])
+    altRouteWithOptions = service.alternateRoute(locations=['Lancaster, PA', 'York, PA'], maxRoutes=2, timeOverage=26)
+    
+    altRouteWithDefaultOptions = service.alternateRoute(locations=locations, options=options)
+    altRouteWithDefaultAndCustomOpts = service.alternateRoute(locations=locations, maxRoutes=2, timeOverage=26, options=options)
+
+
+    # route matrix
+    
+    #This function can create a MATRIX of distances between points in the locations.
+    #Each ROW and COL in the matrix can be multiplied by the factor of how important
+    #the load weight is, multiplied by the specific load weight for that drop.
+    routeMatrix = service.routeMatrix(locations=locations,allToAll=True)
+
